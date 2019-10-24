adcf321420b56c8b5c009cf5c6d82f63221006ec
liud <18729593910@163.com>
2019-10-24 21:36:06 +0800

    提交文件

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace childbirth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint [] childbirths={1,2,3,4};
+            foreach(uint temp in childbirths)
+            {
+               Console .WriteLine(temp);            
+            }
+            Console.ReadLine();
+        }
+    }
+}
